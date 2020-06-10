de591dffc0d9417a746ebfdb5799e908d87e8644
Szymon Chmielewski <szywski27@gmail.com>
2020-06-10 23:25:00 +0200

    add functionalities

--- src/Centus/MainScene.java
+++ src/Centus/MainScene.java
@@ -1,23 +1,50 @@
 package Centus;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.*;
 
-public class MainScene {
+
+public class MainScene extends Stage {
+
+    LoginScene loginScene = new LoginScene();
+
+    String userId;
+
+    @FXML
+    Button showIdBtn;
+    @FXML
+    Button loginBtn;
+
+
+
+
 
 
     @FXML
-    Pane startPane;
+    public void Login(ActionEvent e) throws Exception {
+
+        Stage stage = new Stage();
+        loginScene.start(stage);
 
 
+    }
+
+    public void setId(String s) {
+        this.userId = s;
 
 
+    }
 
-
-
+    public void setUserId() throws IOException {
+        BufferedReader name = new BufferedReader(new FileReader(new File("currentUser.txt")));
+        userId = name.readLine();
+        name.close();
+        File curUs = new File("currentUser.txt");
+        curUs.delete();
+    }
 
 
 }
