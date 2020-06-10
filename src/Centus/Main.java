1d72c7891131684ed40a15c6b027948ce1145a54
Szymon Chmielewski <szywski27@gmail.com>
2020-06-10 23:16:34 +0200

    add instance of MainScene

--- src/Centus/Main.java
+++ src/Centus/Main.java
@@ -1,18 +1,23 @@
 package Centus;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
-       Scene scene = new Scene(root);
-       primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception {
+       MainScene mainScene = new MainScene();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
+        loader.setController(mainScene);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
