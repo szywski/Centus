c064f408ce5c8561b31d763373c8e057cc6fb9b6
Szymon Chmielewski <szywski27@gmail.com>
2020-06-23 23:52:45 +0200

    make logout functionality better

--- src/Centus/MainScene.java
+++ src/Centus/MainScene.java
@@ -9,7 +9,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
 
 
 public class MainScene extends Stage {
@@ -33,17 +35,16 @@ public class MainScene extends Stage {
     private boolean status;
 
     @FXML
-    Button showIdBtn;
-    @FXML
     Button loginBtn;
     @FXML
     MenuBar menuBar;
     @FXML
     Menu revenuesMenu, outgoingsMenu, statisticsMenu;
     @FXML
-    MenuItem addRevenue;
+    MenuItem addRevenue, statisticsItem, addOutgoing;
     @FXML
    private Label userNameLbl;
+
 
     public MainScene instance = null;
 
@@ -80,34 +81,70 @@ public class MainScene extends Stage {
             revenuesScene revenuesScene = new revenuesScene(userId);
             revenuesScene.setUserId(userId);
         }else
-            {Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Information");
-        alert.setHeaderText(null);
-        alert.setContentText("You have to be logged in!");
-        alert.showAndWait();
+            {Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Information");
+                alert.setHeaderText("You have to be logged in");
+                alert.setContentText("Do you want to log in?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK){
+                    Stage stage = (Stage) menuBar.getScene().getWindow();
+                    stage.close();
+                    LoginScene ls = new LoginScene();
+
+                    ls.start(stage);
+
+                }
         }
     }
 
     @FXML
-    public void outgoingsMenu() {
+    public void outM(ActionEvent e) throws Exception {
         if(status != false) {
 
-            outgoingsScene outgoingsScene = new outgoingsScene();
+            outgoingsScene outgoingsScene = new outgoingsScene(userId);
             outgoingsScene.setUserId(userId);
         }else
-        {Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        {Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Information");
-            alert.setHeaderText(null);
-            alert.setContentText("You have to be logged in!");
-            alert.showAndWait();
+            alert.setHeaderText("You have to be logged in");
+            alert.setContentText("Do you want to log in?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                Stage stage = (Stage) menuBar.getScene().getWindow();
+                stage.close();
+                LoginScene ls = new LoginScene();
+
+                ls.start(stage);
+            }
         }
     }
 
 
     @FXML
-    public void statisticsMenu() {
+    public void statisticsMenu() throws Exception {
+        if(status != false) {
 
-    }
+            statisticsScene statisticsScene = new statisticsScene(userId);
+
+        }else
+        {Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Information");
+            alert.setHeaderText("You have to be logged in");
+            alert.setContentText("Do you want to log in?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                Stage stage = (Stage) menuBar.getScene().getWindow();
+                stage.close();
+                LoginScene ls = new LoginScene();
+
+                ls.start(stage);
+
+        }
+    }}
+
     @FXML
     public void logOut(ActionEvent e) throws Exception {
         userId = null;
